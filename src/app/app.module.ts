6e2c01794452fee6250d93bc4765928b35f132c0
Aiden Appleby <aiden.appleby3@gmail.com>
2022-01-01 17:03:16 +0100

    added api calls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { RequestService } from 'src/services/http/request.service';
 
 @NgModule({
   declarations: [
@@ -11,10 +13,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
   providers: [
+    HttpClient,
+    RequestService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/public/lander/lander.component.ts
+++ src/app/public/lander/lander.component.ts
@@ -1,4 +1,7 @@
+import { HttpResponse } from '@angular/common/http';
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { RequestService } from 'src/services/http/request.service';
 
 
 const validSelections = ['checkout', 'checkin', 'lookup'] as const;
@@ -21,10 +24,17 @@ export class LanderComponent implements OnInit, AfterViewInit {
   @ViewChild('duration', {static: false}) duration!:ElementRef
 
   public step = 1;
-  public selectedAction = 'checkout';
+  public selectedAction: string = "";
   public locationAction = 'onsite';
 
-  constructor() { }
+  public user: any = null;
+  public asset: any = null;
+
+  public btnValue = 'Next Step';
+
+  constructor(
+    private request: RequestService
+  ) { }
 
   ngOnInit(): void {
     document.addEventListener('keypress', (e: any) => {
@@ -64,6 +74,24 @@ export class LanderComponent implements OnInit, AfterViewInit {
   focusInput(id: string): void {
     document.getElementById(id)!.focus()
   }
+
+  async validUser(studentID: string): Promise<boolean> {
+    try {
+      const response: any = await this.request.get(`${environment.API_URL}/valid/userByTag/${studentID}`)
+      return response.status === 200;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async validAsset(assetID: string): Promise<boolean> {
+    try {
+      const response: any = await this.request.get(`${environment.API_URL}/valid/assetByTag/${assetID}`)
+      return response.status === 200;
+    } catch (error) {
+      throw error;
+    }
+  }
   
   async valid(step: number): Promise<any> {
     try {
@@ -74,6 +102,12 @@ export class LanderComponent implements OnInit, AfterViewInit {
 
         if (studentID.length === 0) { return {valid: false, message: "missing studentID"}  }
         if (assetID.length === 0) { return {valid: false, message: "missing assetID"} }
+
+        const validUser = await this.validUser(studentID);
+        const validAsset = await this.validAsset(assetID);
+
+        if (!validUser) { return {valid: false, message: "invalid studentID"}  }
+        if (!validAsset) { return {valid: false, message: "invalid assetID"} }
 
         return {valid: true, message: ""}
       }
@@ -103,18 +137,71 @@ export class LanderComponent implements OnInit, AfterViewInit {
 
   async next(): Promise<void> {
     try {
+      if (this.step < 3) {
+        this.btnValue = "";
+      }
       let validator = await this.valid(this.step)
       if (validator.valid) { 
         this.blurInputs();
+        await this.dataRunners();
         this.step++
         this.focusNextInput(this.step)
+        setTimeout(() => {
+          if (this.step <= 2) {
+            this.btnValue = "Next Step"
+          } else {
+            this.btnValue = "Done!"
+          }
+        }, 200)
+
       } else {
         window.alert(validator.message)
+        setTimeout(() => {
+          if (this.step <= 2) {
+            this.btnValue = "Next Step"
+          } else {
+            this.btnValue = "Done!"
+          }
+        }, 200)
         return
       }
       
     } catch (error) {
       console.error(error)
+    }
+  }
+
+  async dataRunners(): Promise<void> {
+    try {
+      if (this.step === 1) {
+        // Step 1 Data Runners
+        let user = await this.getUser()
+        let asset = await this.getAsset()
+        this.user = user;
+        this.asset = asset;
+        console.log(user, asset)
+        return;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getUser(): Promise<any> {
+    try {
+      const response: any = await this.request.get(`${environment.API_URL}/read/userByTag/${this.studentIDInput.nativeElement.value}`)
+      return response.body
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getAsset(): Promise<any> {
+    try {
+      const response: any = await this.request.get(`${environment.API_URL}/read/assetByTag/${this.assetIDInput.nativeElement.value}`)
+      return response.body
+    } catch (error) {
+      throw error;
     }
   }
 
